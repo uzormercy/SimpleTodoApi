63c608ef18c56b20e5a46b31728b70e7eee39be8
Uzor Mercy <uzormercies@gmail.com>
2022-05-31 14:15:42 +0100

    published key

--- config/database.php
+++ config/database.php
@@ -2,12 +2,12 @@
 
 use Illuminate\Support\Str;
 
-$url = parse_url(getenv("DATABASE_URL"));
+// $url = parse_url(getenv("DATABASE_URL"));
 
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
+// $host = $url["host"];
+// $username = $url["user"];
+// $password = $url["pass"];
+// $database = substr($url["path"], 1);
 
 return [
 
